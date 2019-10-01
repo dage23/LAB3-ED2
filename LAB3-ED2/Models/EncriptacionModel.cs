b0eea853f42bce0edba22c1bc71f31d7e2a44531
dage23 <46784507+dage23@users.noreply.github.com>
2019-10-01 17:26:34 -0600

    Creado modelo para encriptacion y declaracion de metodo para encriptar ZZ

---
+++ LAB3-ED2/Models/EncriptacionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LAB3_ED2.Models
+{
+    public class EncriptacionModel
+    {
+        public string EncryptionZigZag(string TextoOriginal, int CantidadNiveles)
+        {
+            return null;
+        }
+    }
+}
